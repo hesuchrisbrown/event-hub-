6cef9a9b603080beaa9e13d371f24b521f53faa7
Michael Jolo <mikegregoryjolo205@gmail.com>
2026-03-11 16:18:56 +0800

    added .env and supabase utils, also integrated with supabase.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-const Input = ({ label, name, type, className, placeholder }) => {
-	return (
-		<fieldset className="fieldset">
-			<legend className="fieldset-legend">{label}</legend>
-			<input
-				name={name}
-				type={type}
-				className={`input ${className}`}
-				placeholder={placeholder}
-			/>
-		</fieldset>
-	);
+const Input = ({ label, name, type, className, placeholder, onChange }) => {
+    return (
+        <fieldset className="fieldset">
+            <legend className="fieldset-legend">{label}</legend>
+            <input
+                name={name}
+                type={type}
+                className={`input ${className}`}
+                placeholder={placeholder}
+                onChange={onChange}
+            />
+        </fieldset>
+    );
 };
 
 export default Input;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,31 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
 import PageWrapper from "../components/PageWrapper";
 import Card from "../components/Card";
 import Input from "../components/Input";
+import supabase from "../utils/supabase";
 
 const Register = () => {
-	return (
-		<PageWrapper>
-			<Header />
-			<Main className="flex justify-center">
-				<div className="flex items-center">
-					<Card>
-						<h1 className="text-xl font-bold">Signup</h1>
-						<Input
-							label="Firstname"
-							name="firstname"
-							type="text"
-							placeholder="Enter your name"
-						/>
-					</Card>
-				</div>
-			</Main>
-			<Footer />
-		</PageWrapper>
-	);
+    const [formData, setformData] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+    });
+
+    const handleInputChange = (event) => {
+        const inputName = event.target.name;
+        const inputValue = event.target.value;
+        setformData({ ...formData, [inputName]: inputValue });
+    };
+
+    const handleSubmit = async () => {
+        const { data, error } = await supabase.auth.signUp({
+            email: formData.email,
+            password: formData.password,
+        });
+
+        if (error) alert(error);
+        if (data) {
+            console.log(data);
+        }
+    };
+
+    return (
+        <PageWrapper>
+            <Header />
+            <Main className="flex justify-center">
+                <div className="flex items-center">
+                    <Card>
+                        <h1 className="text-xl font-bold">Signup</h1>
+
+                        <Input
+                            label="Firstname"
+                            name="firstname"
+                            type="text"
+                            placeholder="Enter your name"
+                            className="w-full"
+                            onChange={handleInputChange}
+                        />
+
+                        <Input
+                            label="Lastname"
+                            name="lastname"
+                            type="text"
+                            placeholder="Enter your lastname"
+                            className="w-full"
+                            onChange={handleInputChange}
+                        />
+
+                        <Input
+                            label="Email"
+                            name="email"
+                            type="text"
+                            placeholder="Enter your Email"
+                            className="w-full"
+                            onChange={handleInputChange}
+                        />
+
+                        <Input
+                            label="Password"
+                            name="password"
+                            type="password"
+                            placeholder="Enter your Password"
+                            className="w-full mb-5"
+                            onChange={handleInputChange}
+                        />
+
+                        <button
+                            className="btn btn-primary rounded-full"
+                            onClick={handleSubmit}
+                        >
+                            Signup
+                        </button>
+                    </Card>
+                </div>
+            </Main>
+            <Footer />
+        </PageWrapper>
+    );
 };
 
 export default Register;
